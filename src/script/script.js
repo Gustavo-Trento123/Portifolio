5c01b2b498350dab4813cb1276ab785dd71f2b60
Gustavo <gustavo.trento@etec.sp.gob.br>
2024-03-12 14:28:44 -0300

    adição das animações

--- src/script/script.js
+++ src/script/script.js
@@ -18,33 +18,34 @@ function alterarDescrição() {
 
     engine.view.opcaoFormacoes.addEventListener('mouseover', function () {
         engine.view.descricaoPaginaInicial.textContent = 'Onde estudei, coisas que aprendi e mais!';
-    });/* Altera o 'competências */
-
-    engine.view.opcaoCompetencias.addEventListener('mouseover', function () {
-        engine.view.descricaoPaginaInicial.classList.add('sair');
-        setTimeout(function() {
-            engine.view.descricaoPaginaInicial.classList.remove('sair');
-            engine.view.descricaoPaginaInicial.classList.add('entrar');}
-        ,2000)
-        
-
-        engine.view.descricaoPaginaInicial.textContent = 'O meu conhecimento na área e habilidades profissionais!';
-    });
+    });/* Altera o 'formações */
 
     engine.view.opcaoCompetencias.addEventListener('mouseover', function () {
         setTimeout(function() {
-            engine.view.descricaoPaginaInicial.classList.remove('sair');
-            engine.view.descricaoPaginaInicial.classList.add('entrar');}
-        ,2000)
+            if(engine.view.descricaoPaginaInicial.classList.contains('sair')){
+                engine.view.descricaoPaginaInicial.classList.remove('sair');
+                engine.view.descricaoPaginaInicial.classList.add('entrar');
+            }else{
+                engine.view.descricaoPaginaInicial.classList.remove('entrar');
+                engine.view.descricaoPaginaInicial.classList.add('sair');
+            }}
+        ,500)
         
         engine.view.descricaoPaginaInicial.textContent = 'O meu conhecimento na área e habilidades profissionais!';
-    });
+    });/* Altera o cométencias */
 
     engine.view.opcaoProjetos.addEventListener('mouseover', function () {
         setTimeout(function() {
-            engine.view.descricaoPaginaInicial.classList.remove('sair');
-            engine.view.descricaoPaginaInicial.classList.add('entrar');}
-        ,2000)
+            if(engine.view.descricaoPaginaInicial.classList.contains('sair')){
+                engine.view.descricaoPaginaInicial.classList.remove('sair');
+                engine.view.descricaoPaginaInicial.classList.add('entrar');
+            }else{
+                engine.view.descricaoPaginaInicial.classList.remove('entrar');
+                engine.view.descricaoPaginaInicial.classList.add('sair');
+                engine.view.descricaoPaginaInicial.classList.remove('sair');
+                engine.view.descricaoPaginaInicial.classList.add('entrar');
+            }}
+        ,500)
         
         engine.view.descricaoPaginaInicial.textContent = 'Meu portifólio de Projetos!';
     });
